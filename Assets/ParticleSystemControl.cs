1cf428969c40c0e7becfff0d0f46b37d8c2ceade
Matthew Hughes <matt.hughes@live.com>
2019-04-26 01:48:16 +0200

    Emitting set up so Kinect can be controlled by Shuriken modules

---
+++ Assets/ParticleSystemControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemControl : MonoBehaviour
+{
+    public ParticleSystem ParticleSystem;
+
+    public void Awake()
+    {
+        if (ParticleSystem == null) ParticleSystem = GetComponent<ParticleSystem>();
+    }
+
+    public float NoiseIntensity
+    {
+        get => ParticleSystem.noise.strength.constant;
+        set
+        {
+            var noiseModule = ParticleSystem.noise;
+            noiseModule.strength = new ParticleSystem.MinMaxCurve(value);
+        }
+    }
+
+    public float BillboardStretch
+    {
+        set
+        {
+            var rendererModule = ParticleSystem.GetComponent<ParticleSystemRenderer>();
+            rendererModule.lengthScale = value;
+            rendererModule.cameraVelocityScale = value;
+            rendererModule.velocityScale = value;
+        }
+    }
+}
