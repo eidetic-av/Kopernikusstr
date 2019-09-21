edcb697e36d5096d4d5198adf8aba0f90435f2a0
Matthew Hughes <matt.hughes@live.com>
2019-09-21 13:04:41 +0200

    TextureTable work

--- Assets/EOC/CustomModules/TextureTable/TextureTable.cs
+++ Assets/EOC/CustomModules/TextureTable/TextureTable.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
+using Eidetic.URack.Base;
+using Eidetic.URack.Base.UI;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
-public class TextureTable : MonoBehaviour
+namespace Eidetic.URack.VFX
 {
-    // Start is called before the first frame update
-    void Start()
+    public class TextureTable : VFXModule
     {
-        
-    }
+        public override VisualEffectAsset TemplateAsset => Resources.Load<VisualEffectAsset>("TextureTable");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Input, TextEntry] public string FolderName = "Discount";
+
+        int imageIndex;
+        [Input, Knob]
+        public float Image
+        {
+            get => imageIndex;
+            set
+            {
+                imageIndex = value.Map(-1, 1, 0, Images.Length - 1).RoundToInt();
+                var image = Images[imageIndex];
+                VisualEffect.SetTexture("Image", image);
+                VisualEffect.SetInt("Width", image.width);
+                VisualEffect.SetInt("Height", image.height);
+            }
+        }
+
+        Texture2D[] images;
+        Texture2D[] Images => images ?? (images = Resources.LoadAll<Texture2D>("Discount"));
     }
 }
