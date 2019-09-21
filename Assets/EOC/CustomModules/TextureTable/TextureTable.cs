9bea12a0eda6c47381a8e0e4c3dba73ed65d19b6
Matthew Hughes <matt.hughes@live.com>
2019-09-21 23:56:35 +0200

    Discount images + texturetable work

--- Assets/EOC/CustomModules/TextureTable/TextureTable.cs
+++ Assets/EOC/CustomModules/TextureTable/TextureTable.cs
@@ -10,24 +10,101 @@ namespace Eidetic.URack.VFX
     {
         public override VisualEffectAsset TemplateAsset => Resources.Load<VisualEffectAsset>("TextureTable");
 
-        [Input, TextEntry] public string FolderName = "Discount";
+        // [Input, TextEntry] public string FolderName = "Discount";
 
-        int imageIndex;
         [Input, Knob]
-        public float Image
+        public float ImageA
         {
-            get => imageIndex;
             set
             {
-                imageIndex = value.Map(-1, 1, 0, Images.Length - 1).RoundToInt();
+                var imageIndex = value.Map(-1, 1, 0, Images.Length - 1).RoundToInt();
                 var image = Images[imageIndex];
-                VisualEffect.SetTexture("Image", image);
-                VisualEffect.SetInt("Width", image.width);
-                VisualEffect.SetInt("Height", image.height);
+                VisualEffect.SetTexture("ImageA", image);
+                VisualEffect.SetUInt("WidthA", (uint)image.width);
+                VisualEffect.SetUInt("HeightA", (uint)image.height);
             }
+        }
+
+        [Input, Knob]
+        public float ImageB
+        {
+            set
+            {
+                var imageIndex = value.Map(-1, 1, 0, Images.Length - 1).RoundToInt();
+                var image = Images[imageIndex];
+                VisualEffect.SetTexture("ImageB", image);
+                VisualEffect.SetUInt("WidthB", (uint)image.width);
+                VisualEffect.SetUInt("HeightB", (uint)image.height);
+            }
+        }
+
+        [Input, Knob]
+        public float Blend
+        {
+            set => VisualEffect.SetFloat("Blend", value.Map(0, 1));
+        }
+
+        [Input(0, 5, 2, 1), Knob]
+        public float ImageScale
+        {
+            set => VisualEffect.SetFloat("ImageScale", value);
         }
 
         Texture2D[] images;
         Texture2D[] Images => images ?? (images = Resources.LoadAll<Texture2D>("Discount"));
+
+        [Input(0, 50, 2, 1), Knob]
+        public float Speed
+        {
+            set => VisualEffect.playRate = value;
+        }
+
+        [Input(0, 1, 1, .2f), Knob]
+        public float Glow
+        {
+            set => VisualEffect.SetFloat("Glow", value);
+        }
+
+        [Input(0, 1, 3, 0), Knob]
+        public float NoiseIntensity
+        {
+            set => VisualEffect.SetFloat("NoiseIntensity", value);
+        }
+        [Input(0, 10, 4, 1), Knob]
+        public float NoiseFrequency
+        {
+            set => VisualEffect.SetFloat("NoiseFrequency", value);
+        }
+        [Input(0, 10, 4, 1), Knob]
+        public float NoiseScaleX
+        {
+            set => VisualEffect.SetFloat("NoiseScaleX", value);
+        }
+        [Input(0, 10, 4, 1), Knob]
+        public float NoiseScaleY
+        {
+            set => VisualEffect.SetFloat("NoiseScaleY", value);
+        }
+
+        [Input(0, 10, 2, 0), Knob]
+        public float Warp
+        {
+            set => VisualEffect.SetFloat("Warp", value);
+        }
+        [Input(0, 1, 4, 1), Knob]
+        public float Shards
+        {
+            set => VisualEffect.SetFloat("WarpShards", value);
+        }
+        [Input, Knob]
+        public float WarpDirectionX
+        {
+            set => VisualEffect.SetFloat("WarpDirectionX", value);
+        }
+        [Input, Knob]
+        public float WarpDirectionY
+        {
+            set => VisualEffect.SetFloat("WarpDirectionY", value);
+        }
     }
 }
